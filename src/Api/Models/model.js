16cc4da726cd1e76fb4dab58671b28aa542e3c75
yehia <anaa76746@gmail.com>
2024-12-03 00:35:10 +0200

    Rename for many files

---
+++ src/Api/Models/model.js
@@ -0,0 +1,2 @@
+//* this folder to add all DB schems you can remove this file
+//* each obj will have its own schema at its own file
---
+++ src/Api/Routes/route.js
@@ -0,0 +1,2 @@
+//* this folder to add all routes you can remove this file
+//* each route will have its own  file
