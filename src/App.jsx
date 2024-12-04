c772dcbdc21ff7014ccd8059899cd95f9381ef06
Amryasser456 <amryasser3072004@gmail.com>
2024-12-04 15:05:12 +0200

    add css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
+import React from "react";
+    
 
 function App() {
-  return (
-    <div>
-    <h1>Hello World</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>   
 }
 
 export default App;
